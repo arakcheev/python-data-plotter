a604ee6e7e6a09bd5baa575cda523ad305e13d3c
Artem Arakcheev <artem.ft@gmail.com>
2016-04-25 14:49:27 +0300

    Kic96 plot

--- Parameters.py
+++ Parameters.py
@@ -13,13 +13,20 @@ class Parameters:
         pass
 
     sunRadius = Config.getfloat("sun", "radius")
-    planet_radius = Config.getfloat("planet", "radius") * sunRadius
+    earthRadius = Config.getfloat("earth", "radius")
+    earthMass = Config.getfloat("earth", "mass")
     sunMass = Config.getfloat("sun", "mass")
+
+    planet_radius = Config.getfloat("planet", "radius") * earthRadius
+
     dmass_1 = Config.getfloat("common", "dmass_1") * sunMass
-    dmass_2 = Config.getfloat("common", "dmass_2")  # 0.0006*dmsun
+    dmass_2 = Config.getfloat("common", "dmass_2") * earthMass
+    
     q_prop = dmass_1 / dmass_2
     prop = q_prop / (q_prop + 1.0)
+
     gravitationMass = Config.getfloat("common", "gravity") * (dmass_1 + dmass_2)
-    ab = Config.getfloat("planet", "separation")  # /rpl!10.1*rsun
+    ab = Config.getfloat("planet", "separation") * 1.49597892e13
+    
     omega = math.sqrt(gravitationMass / ab ** 3)
     period = 2. * math.pi / omega
